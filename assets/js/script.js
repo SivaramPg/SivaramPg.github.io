78dea86a97c6c27a995b62a45061898f1e4c5e31
SivaramPg <sivarampg95@gmail.com>
2018-05-29 20:33:04 +0530

    trying to add animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 // Function for changing the navbar background to black on click of navbar-toggle button
 const navbar = document.getElementById("navbar");
 const changeNavBg = (param = navbar) => { param.classList.toggle("navbgblackonclick") };
+
+
