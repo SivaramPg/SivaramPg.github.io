8c1ff730cb40511bf5279a6c0d38a5a5f70273d4
SivaramPg <sivarampg95@gmail.com>
2018-05-31 11:44:41 +0530

    Landing tagline change

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # SivaramPg.github.io
 
-This is my Github repository hosting my personal website.
-
-It is a work in progress and I am striving towards fulfilling my dream to be a full stack developer for a reputed employer.
-
-It is going to be fun journey Wish me Luck.
+My Portfolio Website.
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,29 @@
-// Function for changing the navbar background to black on click of navbar-toggle button
+//
+////// caching DOM items
+//
+const aboutMeNavButton = document.getElementById("aboutme");
+const contactMeNavButton = document.getElementById("contactme");
+const portfolioNavButton = document.getElementById("portfolio");
 const navBarToggler = document.getElementById("navbar-toggler");
 const navbar = document.getElementById("navbar");
+
+//
+////// Functions
+//
+
+// Function for changing the navbar background to black on click of navbar-toggle button
 const changeNavBg = () => navbar.classList.toggle("navbgblackonclick") ;
 
-navBarToggler.addEventListener("click", changeNavBg );
+//Functions for taking to different sections of website on click of respective navbar button...... Some more could be added later.
+// const goToAbout = () => html.body.animate({scrollTop: .section1});
+// const goToContact = () => ;
+// const goToPortfolio = () => ;
 
+//
+////// Event Listeners on Elements
+// 
+navBarToggler.addEventListener("click", changeNavBg );
+// aboutMeNavButton.addEventListener("click", goToAbout);
+// contactMeNavButton.addEventListener("click", goToContact);
+// portfolioNavButton.addEventListener("click", goToPortfolio);
 
