0edc29a1e4bfa2b3f553cfa23c949fa324839f8b
SivaramPg <sivaramp95@gmail.com>
2018-06-08 20:03:41 +0530

    visual and ux improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 //
 ////// caching DOM items
 //
+const goTop = document.getElementById("top");
+const goBottom = document.getElementById("bottom");
 const aboutMeNavButton = document.getElementById("aboutme");
 const contactMeNavButton = document.getElementById("contactme");
 const portfolioNavButton = document.getElementById("portfolio");
 const navBarToggler = document.getElementById("navbar-toggler");
 const navbar = document.getElementById("navbar");
+const scrollDown = document.getElementById("scrolldown")
 const aboutMeSection = document.getElementById("aboutmesection");
 const contactMeSection = document.getElementById("contactmesection");
 const portfolioSection = document.getElementById("portfoliosection");
-
+const beginning = document.getElementById("beginning");
+const ending = document.getElementById("ending");
 //
 ////// Functions
 //
@@ -21,12 +25,15 @@ const changeNavBg = () => navbar.classList.toggle("navbgblackonclick") ;
 const goToAbout = () => aboutMeSection.scrollIntoView({behavior: 'smooth', block:'start'});
 const goToContact = () => contactMeSection.scrollIntoView({behavior: 'smooth', block:'start'});
 const goToPortfolio = () => portfolioSection.scrollIntoView({behavior: 'smooth', block:'start'});
-
+const goToTop = () => beginning.scrollIntoView({behavior: 'smooth', block:'start'});
+const goToBottom = ()=> ending.scrollIntoView({behavior: 'smooth', block:'start'});
 //
 ////// Event Listeners on Elements
 // 
+goTop.addEventListener("click", goToTop);
+goBottom.addEventListener("click", goToBottom);
 navBarToggler.addEventListener("click", changeNavBg );
 aboutMeNavButton.addEventListener("click", goToAbout);
 contactMeNavButton.addEventListener("click", goToContact);
 portfolioNavButton.addEventListener("click", goToPortfolio);
-
+scrollDown.addEventListener("mouseover", goToAbout)
