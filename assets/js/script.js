113f34c524b280e070743d22b772c66f84df84d6
SivaramPg <sivarampg95@gmail.com>
2021-02-14 19:33:18 +0530

    format files with prettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,115 +1,117 @@
-//
-////// caching DOM items
-//
+/**
+ * caching DOM items
+ */
 
-const homeNavButton = document.getElementById("home__link");
-const aboutMeNavButton = document.getElementById("about__link");
-const skillsNavButton = document.getElementById("skills__link");
-const portfolioNavButton = document.getElementById("portfolio__link");
+const homeNavButton = document.getElementById('home__link');
+const aboutMeNavButton = document.getElementById('about__link');
+const skillsNavButton = document.getElementById('skills__link');
+const portfolioNavButton = document.getElementById('portfolio__link');
 
-const navBarToggler = document.getElementById("navbar-toggler");
-const navbar = document.getElementById("navbar");
+const navBarToggler = document.getElementById('navbar-toggler');
+const navbar = document.getElementById('navbar');
 
-const downPointer = document.getElementById("scrollout");
-const goTop = document.getElementById("scrollhome");
+const downPointer = document.getElementById('scrollout');
+const goTop = document.getElementById('scrollhome');
 
-const header = document.getElementById("header");
-const aboutMeSection = document.getElementById("about");
-const skillsSection = document.getElementById("skills");
-const portfolioSection = document.getElementById("portfolio");
+const header = document.getElementById('header');
+const aboutMeSection = document.getElementById('about');
+const skillsSection = document.getElementById('skills');
+const portfolioSection = document.getElementById('portfolio');
 
-//
-////// Functions
-//
+/**
+ * Function for changing the navbar background to black on click of navbar-toggle button
+ */
+const changeNavBg = () => navbar.classList.toggle('navbgblackonclick');
 
-// Function for changing the navbar background to black on click of navbar-toggle button
-const changeNavBg = () => navbar.classList.toggle("navbgblackonclick");
-
-//Functions for taking to different sections of website on click of respective navbar button...... Some more could be added later.
+/**
+ * Functions for taking to different sections of website on click of respective navbar button...... Some more could be added later.
+ */
 const goToAbout = () => {
-  aboutMeSection.scrollIntoView({ behavior: "smooth", block: "start" });
+  aboutMeSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
 };
 
 const goToSkills = () => {
-  skillsSection.scrollIntoView({ behavior: "smooth", block: "center" });
+  skillsSection.scrollIntoView({ behavior: 'smooth', block: 'center' });
 };
 
 const goToPortfolio = () => {
-  portfolioSection.scrollIntoView({ behavior: "smooth", block: "start" });
+  portfolioSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
 };
 
 const goToTop = () => {
-  header.scrollIntoView({ behavior: "smooth", block: "start" });
+  header.scrollIntoView({ behavior: 'smooth', block: 'start' });
 };
 
-// Function to change display property of the goToTop button
+/**
+ * Function to change display property of the goToTop button
+ */
 const hideOnScroll = () => {
   if (
     document.body.scrollTop > 350 ||
     document.documentElement.scrollTop > 350
   ) {
     if (screen.availWidth <= 768) {
-      goTop.style.display = "none";
+      goTop.style.display = 'none';
     } else {
-      goTop.style.display = "block";
+      goTop.style.display = 'block';
     }
   } else {
-    goTop.style.display = "none";
+    goTop.style.display = 'none';
   }
 };
 
-//
-// //// Event Listeners on Elements
-//
+/**
+ * Event Listeners on Elements
+ */
 
-window.addEventListener("scroll", hideOnScroll);
-window.addEventListener("load", hideOnScroll);
-window.addEventListener("resize", hideOnScroll);
-navBarToggler.addEventListener("click", changeNavBg);
-homeNavButton.addEventListener("click", goToTop);
-aboutMeNavButton.addEventListener("click", goToAbout);
-skillsNavButton.addEventListener("click", goToSkills);
-portfolioNavButton.addEventListener("click", goToPortfolio);
-goTop.addEventListener("click", goToTop);
-downPointer.addEventListener("mouseover", goToAbout);
+window.addEventListener('scroll', hideOnScroll);
+window.addEventListener('load', hideOnScroll);
+window.addEventListener('resize', hideOnScroll);
+navBarToggler.addEventListener('click', changeNavBg);
+homeNavButton.addEventListener('click', goToTop);
+aboutMeNavButton.addEventListener('click', goToAbout);
+skillsNavButton.addEventListener('click', goToSkills);
+portfolioNavButton.addEventListener('click', goToPortfolio);
+goTop.addEventListener('click', goToTop);
+downPointer.addEventListener('mouseover', goToAbout);
 
 $(window).scroll(function logovis() {
   if ($(window).scrollTop() >= 350) {
-    $(".section--about__image").css("visibility", "visible");
-    $(".section--about__image").animate(
+    $('.section--about__image').css('visibility', 'visible');
+    $('.section--about__image').animate(
       {
-        left: "0px",
-        opacity: "1"
+        left: '0px',
+        opacity: '1',
       },
       800,
-      "linear"
+      'linear'
     );
-    $(".section--about__image").off("scroll");
+    $('.section--about__image').off('scroll');
   }
 });
 
 $(window).scroll(function introvis() {
   if ($(window).scrollTop() >= 700) {
-    $(".section--about__intro").css("visibility", "visible");
-    $(".section--about__intro").animate(
+    $('.section--about__intro').css('visibility', 'visible');
+    $('.section--about__intro').animate(
       {
-        opacity: "1"
+        opacity: '1',
       },
       800,
-      "linear"
+      'linear'
     );
-    $(".section--about__intro").off("scroll");
+    $('.section--about__intro').off('scroll');
   }
 });
 
-$(window).scroll(function() {
+$(window).scroll(function () {
   if ($(window).scrollTop()) {
-    $("nav").removeClass("bg-none");
-    $("nav").addClass("bg-dark");
-    $(".navbar__style").css("boxShadow", "0 0 10px 1px black");
+    $('nav').removeClass('bg-none');
+    $('nav').addClass('bg-dark');
+    $('.navbar__style').css('boxShadow', '0 0 10px 1px black');
   } else {
-    $("nav").addClass("bg-none");
-    $("nav").removeClass("bg-dark");
-    $(".navbar__style").css("boxShadow", "0 0 0 0 transparent");
+    $('nav').addClass('bg-none');
+    $('nav').removeClass('bg-dark');
+    $('.navbar__style').css('boxShadow', '0 0 0 0 transparent');
   }
 });
